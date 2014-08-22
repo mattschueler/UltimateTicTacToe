25e9e5e24d9709fd9104447e41e44de95930b20c
Hima <himat15@yahoo.com>
2014-08-22 16:02:32 -0400

    The game can finally detect the end :heart: :kiss:
    You're welcome

--- src/com/auronic/TTTSpace.java
+++ src/com/auronic/TTTSpace.java
@@ -13,6 +13,35 @@ public class TTTSpace extends JButton {
 	public Coords xy;
 	
 	public TTTSpace(int i, int j, int k, int l) {
+		
+			   
+		    double answer, top, bottom, terms, increment;  
+		    terms=Math.abs(0);  
+		    top=Math.abs(1);  
+		    bottom=Math.abs(1);  
+		    answer=Math.abs(1);  
+		    increment=Math.abs(1);  
+		      
+		          
+		    while (answer != 2.71828183){  
+		    //for(int i=1;i<25;i++){  
+		        answer+=(top/bottom);  
+		        increment++;  
+		        bottom*=increment;  
+		        terms++;  
+		          
+		        //System.out.println("#:"+increment+" "+bottom+". Answer:"+answer);  
+		        //System.out.println(answer);  
+		          
+		    }
+		    System.out.println("The amount of terms needed is: "+terms);  
+		    /* 
+		    System.out.println(answer); 
+		    System.out.println(increment); 
+		    System.out.println(bottom); 
+		    */  
+		    
+		  
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {

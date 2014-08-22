378e5413e07ee54c00f3dec33da2346846ff1036
mattschueler <mjschueler@gmail.com>
2014-08-22 15:36:24 -0400

    Board to show who wins each box
    set up small board on side to show who won in each big box

--- src/com/auronic/UTTTGUI.java
+++ src/com/auronic/UTTTGUI.java
@@ -1,11 +1,15 @@
 package com.auronic;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class UTTTGUI {
 
 	public static JFrame window;
 	public static Board board;
+	public static WinBoard wBoard;
 	public static JSplitPane boards;
 	
 	public UTTTGUI() {
@@ -14,11 +18,19 @@ public class UTTTGUI {
 	
 	public static void createAndShowGUI() {
 		window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		board = new Board();
-		window.add(board);
+		board.setMinimumSize(new Dimension(660,650));
+		board.setMaximumSize(new Dimension(660,650));
+		board.setPreferredSize(new Dimension(660,650));
+		wBoard = new WinBoard();
+		wBoard.setMinimumSize(new Dimension(120,120));
+		wBoard.setMaximumSize(new Dimension(120,120));
+		wBoard.setPreferredSize(new Dimension(120,120));
+		boards = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, board, wBoard);
+		boards.setEnabled(false);
+		window.getContentPane().add(boards);
 		window.pack();
-		window.validate();
-		window.setSize(955,690);
 		window.setVisible(true);
 		window.setResizable(false);
 	}
--- src/com/auronic/WinBoard.java
+++ src/com/auronic/WinBoard.java
@@ -1,17 +1,50 @@
 package com.auronic;
 
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.HashMap;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
-public class WinBoard {
+public class WinBoard extends JPanel {
 
-	private GridLayout gl;
-	public HashMap<String, JLabel> spaces;
+	private SpringLayout sl;
+	public HashMap<String, JLabel> bigSpaces;
 	
 	public WinBoard() {
-		gl = new GridLayout();
+		sl = new SpringLayout();
+		bigSpaces = new HashMap<String, JLabel>();
+		this.setLayout(sl);
+		int i, j;
+		for (i=0;i<3;i++) {
+			for (j=0;j<3;j++) {
+				bigSpaces.put(String.format("" + i + j), new JLabel("abcde"));
+			}
+		}
+		for (i=0;i<3;i++) {
+			for (j=0;j<3;j++) {
+				if (i == 0) {
+					sl.putConstraint(SpringLayout.WEST, bigSpaces.get(String.format("" + i + j)), 10, SpringLayout.WEST, this);
+				} else {
+					sl.putConstraint(SpringLayout.WEST, bigSpaces.get(String.format("" + i + j)), 10, SpringLayout.EAST, bigSpaces.get(String.format("" + (i-1) + j)));
+				}
+				if (j == 0) {
+					sl.putConstraint(SpringLayout.NORTH, bigSpaces.get(String.format("" + i + j)), 10, SpringLayout.NORTH, this);
+				} else {
+					sl.putConstraint(SpringLayout.NORTH, bigSpaces.get(String.format("" + i + j)), 10, SpringLayout.SOUTH, bigSpaces.get(String.format("" + i + (j-1))));
+				}
+				bigSpaces.get(String.format("" + i + j)).setPreferredSize(new Dimension(80,80));
+				bigSpaces.get(String.format("" + i + j)).setFont(new Font("Arial", Font.PLAIN, 28));
+				this.add(bigSpaces.get(String.format("" + i + j)));
+			}
+		}
 	}
 	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(400,400);
+	}
 }
